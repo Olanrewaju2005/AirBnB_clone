b84597079aca5c74ee88b1d4a70734595192f142
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-02-09 02:33:21 -0800

    second commit

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,22 @@ import uuid
 
 def BaseModel:
    
-    def __init__(self, name, my_number):
-        self.name = name
-	self.my_number = int(my_number)
-	self.id = str(uuid.uuid4())
-	self.created_at = datetime.datetime.now()
-	self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        datetime_isoformat = "%Y-%m-%dT%H:%M:%S.%"
+        if kwargs:
+            for key, value in kwargs:
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, datetime_isoformat)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.name = name
+            self.my_number = int(my_number)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}]({self.id}){self.__dict__}"
