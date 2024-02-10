00ef02953ae91af22253668ced50e42d6b74d56c
root <root@Buffalo>
2024-02-10 17:19:23 +0100

    class documentations

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,21 @@ import uuid
 def BaseModel:
    
     def __init__(self, *args, **kwargs):
+        """
+        Initialiazes new instance of BaseModel.
+
+        Args:
+            *args: Unused positional arguments
+            **kwargs: Dictionary representation of an instance.
+
+        If kwargs is not empty:
+            Each key has an attribute name
+            Each value is the value of the corresponding attr name
+            Convert datetime to datetime objects
+
+        Otherwise:
+            Create id and created_at values as initially done
+        """
         datetime_isoformat = "%Y-%m-%dT%H:%M:%S.%"
         if kwargs:
             for key, value in kwargs:
@@ -23,13 +38,24 @@ def BaseModel:
             self.updated_at = datetime.datetime.now()
 
     def __str__(self):
+        """Returns a string representation of the object class"""
         return f"[{self.__class__.__name__}]({self.id}){self.__dict__}"
 
     def save(self):
-            updated_at = datetime.date.time.now()
+        """
+        Updates the public instance attribute update_at
+        with the current datetime
+        """
+        updated_at = datetime.date.time.now()
 
     def to_dict(self):
+        """
+        Returns a dictionary containing key/value
+        of __dict__ for an instance
+        """
         self.__dict__['__class__'] = self.__class__.__name__
 	self.__dict__['created_at'] = self.created_at.isoformat()
         self.__dict__['updated_at'] = self.updated_at.isoformat()
         return self.__dict__.items()
+
+
